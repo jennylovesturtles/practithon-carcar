d0434e3ced06ec7f97f0ce6ec5b06ce5de73a9c2
jenny <jennyyin2s@gmail.com>
2023-02-17 20:05:11 -0600

    Added dog and adoption form models

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=20)
+    intake_date = models.DateTimeField(auto_now_add=True)
+    fertile = models.BooleanField()
+    picture = models.URLField(null=True)
+    status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} {self.age}"
+
+class AdoptionForm(models.Model):
+    name = models.CharField(max_length=200)
+    dog_id = models.IntegerField()
+    application_status = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} application status for dog: {self.dog_id} is: {self.application_status}"
